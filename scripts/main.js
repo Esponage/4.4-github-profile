abecb4bb121231008fcd4c79bd38a71b3b0ccc36
Michael Esposito <michaelde1984@gmail.com>
2015-09-18 15:09:45 -0400

    linked in repositories, linked in most of sidebar

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,24 @@ $(document).ready(function() {
   })
   .then(function(response) {
         console.log(response);
+        response.forEach(function(repo) {
+        $(".repositories").append('<li>' + repo.html_url + '</li>');
+      });
 });
     });
+
+  $(document).ready(function() {
+    $.ajax({
+      url: "https://api.github.com/users/esponage/starred",
+      type: "GET",
+      dataType: "json",
+      headers: { "Authorization": "token 58734ea7b7318b9a258a4b86ea14c7c0f291f75c" }
+    })
+     .then(function(response) {
+      console.log(response.length);
+      $(".js-starred").html(response.length);
+    });
+  });
 
 $(document).ready(function() {
     $.ajax({
@@ -18,6 +34,12 @@ $(document).ready(function() {
         headers: { "Authorization": "token 58734ea7b7318b9a258a4b86ea14c7c0f291f75c" }
       }).then(function(response) {
           console.log(response);
-
+          $(".js-user-photo").html('<img src=" '  + response.avatar_url + ' "/> ');
+          $(".js-name").html(response.name);
+          $(".js-user-name").html(response.login);
+          $(".js-email").html(response.email);
+          $(".js-date-joined").html(response.created_at);
+          $(".js-followers").html(response.followers);
+          $(".js-following").html(response.following);
       });
     });
